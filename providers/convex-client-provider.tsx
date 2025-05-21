c7900ffe5e0933755cdf40b3618dae7ec35eef12
Alexander <105300503+AlexWayke@users.noreply.github.com>
2025-05-21 18:09:16 +0400

    Feat: added features

--- providers/convex-client-provider.tsx
+++ providers/convex-client-provider.tsx
@@ -3,10 +3,11 @@
 import { ClerkProvider, useAuth } from "@clerk/nextjs";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
 import {
-  AuthLoading,
   Authenticated,
+  AuthLoading,
   ConvexReactClient
 } from "convex/react";
+import { Loading } from "@/components/auth/loading";
 
 interface ConvexClientProviderProps {
   children: React.ReactNode;
@@ -18,9 +19,14 @@ const convex = new ConvexReactClient(convexUrl);
 
 export const ConvexClientProvider = ({children}: ConvexClientProviderProps) => {
   return (
-    <ClerkProvider>
+    <ClerkProvider publishableKey="pk_test_Y3VkZGx5LXdyZW4tMzMuY2xlcmsuYWNjb3VudHMuZGV2JA">
       <ConvexProviderWithClerk useAuth={useAuth} client={convex}>
-        {children}
+        <Authenticated>
+          {children}
+        </Authenticated>
+        <AuthLoading>
+          <Loading/>
+        </AuthLoading>
       </ConvexProviderWithClerk>
     </ClerkProvider>
   )
