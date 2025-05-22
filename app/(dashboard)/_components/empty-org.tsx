a38b442c5a76009830a4964cdb21802034968316
Alexander <105300503+AlexWayke@users.noreply.github.com>
2025-05-22 16:58:19 +0400

    Feat: added empty org page

---
+++ app/(dashboard)/_components/empty-org.tsx
@@ -0,0 +1,25 @@
+import Image from 'next/image';
+
+import { CreateOrganization } from '@clerk/nextjs';
+import { Button } from '@/components/ui/button';
+import { DialogContent, Dialog, DialogTrigger } from '@/components/ui/dialog';
+
+export const EmptyOrg = () => {
+  return (
+    <div className="h-full flex flex-col items-center justify-center">
+      <Image src="/empty-org.svg" alt="Empty" height={180} width={180} />
+      <h2 className="text-2xl font-semibold mt-6">Welcome to Board</h2>
+      <p className="text-muted-foreground text-sm mt-2">Create organization to get started</p>
+      <div className="mt-6 ">
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button size="lg">Create organization</Button>
+          </DialogTrigger>
+          <DialogContent className="p-0 bg-transparent">
+            <CreateOrganization />
+          </DialogContent>
+        </Dialog>
+      </div>
+    </div>
+  );
+};
--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -13,7 +13,7 @@ const DashboardLayout = ({ children }: DashboardLayoutProps) => {
       <div className="pl-[60px] h-full">
         <div className="flex gap-x-3 h-full">
           <OrgSidebar />
-          <div className="h-4 flex-1">
+          <div className="h-full flex-1">
             <Navbar />
             {children}
           </div>
